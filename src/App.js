a12232e19f07f1885085e75f20e269024c832a50
igorleb <ip.lebedev07@gmail.com>
2022-05-23 21:39:18 +0400

    feat: add draggable cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
+import {v4 as uuidv4} from 'uuid';
+import {randomColor} from 'randomcolor';
+import Draggable from 'react-draggable';
 
 function App() {
   const [item, setItem] = useState('')
@@ -11,12 +14,50 @@ function App() {
     localStorage.setItem('items', JSON.stringify(items))
   }, [items])
 
+  const newItem = () => {
+    if (item.trim() !== '') {
+      const newItem = {
+        id: uuidv4(),
+        item: item,
+        color: randomColor({
+          luminosity: 'light'
+        }),
+        defaultPos: {
+          x: 500,
+          y: -500
+        }
+
+      }
+      setItems((items) => [...items, newItem])
+      setItem('')
+    } else {
+      alert('Enter something...')
+      setItem('');
+    }
+  }
+
   return (
     <div className='App'>
       <div className='wrapper'>
-        <input type='text' placeholder='Enter something...' />
-        <button className='enter'>ENTER</button>
+        <input type='text' placeholder='Enter something...' onChange={(e) => setItem(e.target.value)}/>
+        <button className='enter' onClick={newItem}>ENTER</button>
       </div>
+      {
+        items.map((item, index) => {
+          return (
+            <Draggable 
+              key={index}
+              defaultPosition={item.defaultPos}
+              >
+                <div className='todo_item'>
+                  {`${item.item}`}
+                  <button className='delete'>X</button>
+                </div>
+              
+            </Draggable>
+          )
+        })
+      }
     </div>
   );
 }
