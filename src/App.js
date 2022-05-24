34b6792c3f35da9a64551c78152a2bd9ead02242
igorleb <ip.lebedev07@gmail.com>
2022-05-24 10:00:53 +0400

    feat: implement card deletion

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {v4 as uuidv4} from 'uuid';
-import {randomColor} from 'randomcolor';
+import { v4 as uuidv4 } from 'uuid';
+import { randomColor } from 'randomcolor';
 import Draggable from 'react-draggable';
 
 function App() {
-  const [item, setItem] = useState('')
+  const [item, setItem] = useState('');
   const [items, setItems] = useState(
     JSON.parse(localStorage.getItem('items')) || []
-  )
+  );
 
   useEffect(() => {
-    localStorage.setItem('items', JSON.stringify(items))
-  }, [items])
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
 
   const newItem = () => {
     if (item.trim() !== '') {
@@ -20,44 +20,47 @@ function App() {
         id: uuidv4(),
         item: item,
         color: randomColor({
-          luminosity: 'light'
+          luminosity: 'light',
         }),
         defaultPos: {
           x: 500,
-          y: -500
-        }
-
-      }
-      setItems((items) => [...items, newItem])
-      setItem('')
+          y: -500,
+        },
+      };
+      setItems(items => [...items, newItem]);
+      setItem('');
     } else {
-      alert('Enter something...')
+      alert('Enter something...');
       setItem('');
     }
+  };
+
+  const deleteNode = (id) => {
+    setItems(items.filter((item) => item.id !== id))
   }
 
   return (
     <div className='App'>
       <div className='wrapper'>
-        <input type='text' placeholder='Enter something...' onChange={(e) => setItem(e.target.value)}/>
-        <button className='enter' onClick={newItem}>ENTER</button>
+        <input
+          type='text'
+          placeholder='Enter something...'
+          onChange={e => setItem(e.target.value)}
+        />
+        <button className='enter' onClick={newItem}>
+          ENTER
+        </button>
       </div>
-      {
-        items.map((item, index) => {
-          return (
-            <Draggable 
-              key={index}
-              defaultPosition={item.defaultPos}
-              >
-                <div className='todo_item' style={{backgroundColor: item.color}}>
-                  {`${item.item}`}
-                  <button className='delete'>X</button>
-                </div>
-              
-            </Draggable>
-          )
-        })
-      }
+      {items.map((item, index) => {
+        return (
+          <Draggable key={index} defaultPosition={item.defaultPos}>
+            <div className='todo_item' style={{ backgroundColor: item.color }}>
+              {`${item.item}`}
+              <button className='delete' onClick={() => deleteNode(item.id)}>X</button>
+            </div>
+          </Draggable>
+        );
+      })}
     </div>
   );
 }
